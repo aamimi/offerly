16844592978780e3da7edf6c26378714a6bea83a
aamimi <amimi.achref@gmail.com>
2025-02-07 02:48:29 -0500

    feat: use category meta title and meta description

--- src/pages/CategoryDetails.tsx
+++ src/pages/CategoryDetails.tsx
@@ -1,10 +1,18 @@
 import {fetchCategoriesBySlug} from "@api/categories";
 import {useParams} from "react-router-dom";
 import {useQuery} from "@tanstack/react-query";
-import {ErrorMessage, LoadingSpinner} from "@components/QueryWrapper.tsx";
+import {ErrorMessage, LoadingSpinner} from "@components/QueryWrapper";
 import SubCategoryType from "@components/category/SubCategoryType";
-import SubCategory from "@components/category/SubCategory.tsx";
+import SubCategory from "@components/category/SubCategory";
 import {Helmet} from "react-helmet";
+
+interface ICategory {
+    slug: string;
+    name: string;
+    meta_title: string;
+    meta_description: string | null | undefined;
+    subcategories: SubCategoryType[];
+}
 
 const CategoryDetails = () => {
     const {slug} = useParams<{ slug: string }>();
@@ -12,7 +20,8 @@ const CategoryDetails = () => {
         queryKey: ['category', slug],
         queryFn: () => fetchCategoriesBySlug(slug ?? ''),
     });
-    const category = response?.data;
+
+    const category: ICategory = response?.data;
 
     if (isLoading) return <LoadingSpinner/>;
     if (isError) return <ErrorMessage error={error.message}/>;
@@ -20,8 +29,8 @@ const CategoryDetails = () => {
     return (
         <>
             <Helmet>
-                <title>Categories - {category.name}</title>
-                <meta name="description" content="Browse category details." />
+                <title>Categories - {category.meta_title}</title>
+                <meta name="description" content={category.meta_description ?? ''}/>
             </Helmet>
             <div className="container mx-auto">
                 <div className="card">
